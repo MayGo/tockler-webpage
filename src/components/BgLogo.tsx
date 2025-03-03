33480c8c1835de43b43b4ba86cce319b9e7ba7da
Maigo Erit <maigo.erit@gmail.com>
2025-03-03 22:09:51 +0200

    Adjust BgLogo component positioning for better layout alignment
    - Add `right` property to center background logo
    - Fine-tune logo positioning with `right` and `transform` properties

--- src/components/BgLogo.tsx
+++ src/components/BgLogo.tsx
@@ -5,7 +5,7 @@ import { TocklerLogo } from './Header/TocklerLogo';
 
 export function BgLogo() {
     return (
-        <Box position="absolute" top="100px" transform="translateX(10%)" zIndex={1} overflow="hidden">
+        <Box position="absolute" top="100px" right="50%" transform="translateX(10%)" zIndex={1} overflow="hidden">
             <TocklerLogo boxSize="800px" color="primary" opacity={0.5} />
         </Box>
     );
